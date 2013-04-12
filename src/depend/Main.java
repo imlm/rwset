989fb33ffd095df1ac15734f9b3bfa9ebc8417b4
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2013-04-12 13:26:54 -0300

    adding reportType option to script run

--- src/depend/Main.java
+++ src/depend/Main.java
@@ -1,11 +1,14 @@
 package depend;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Set;
 
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.examples.drivers.PDFTypeHierarchy;
 import com.ibm.wala.types.ClassLoaderReference;
 import com.ibm.wala.types.Selector;
 import com.ibm.wala.types.TypeReference;
@@ -13,6 +16,7 @@ import com.ibm.wala.util.CancelException;
 import com.ibm.wala.util.WalaException;
 import com.ibm.wala.util.io.CommandLine;
 import com.ibm.wala.util.warnings.Warnings;
+import com.ibm.wala.viz.DotUtil;
 
 import depend.util.Util;
 
@@ -54,17 +58,65 @@ public class Main {
     if (method == null) {
       throw new RuntimeException("could not find class!");
     }
-    System.out.printf("data dependencies to method %s\n", method);
+    
     
     // looking for dependencies to method
     Set<IMethod> set = an.getDependencies(method, false, false);
     
-    // printing dependencies
-    for (IMethod m : set) {
-      if (Util.isAppClass(m.getDeclaringClass())) {
-        System.out.printf("  %s\n", m);
-      }      
-    }
+    String reportType = Util.getStringProperty("reportType").trim();
+    
+    dumpResults(method, set, reportType);
 
+  }
+
+  private static void dumpResults(IMethod method, Set<IMethod> set,
+      String reportType) throws IOException, WalaException {
+    if (reportType.equals("list")) {
+      
+      System.out.printf("data dependencies to method %s\n", method);
+
+      // printing dependencies
+      for (IMethod m : set) {
+        if (Util.isAppClass(m.getDeclaringClass())) {
+          System.out.printf("  %s\n", m);
+        }      
+      }
+
+    } else if (reportType.equals("dot")) {
+      //TODO: you may want to print before propagating 
+      //data dependencies
+      
+      /**
+       * generate dot
+       */
+      StringBuffer sb = new StringBuffer();
+      sb.append("digraph \"DirectedGraph\" {\n");
+      sb.append(" graph [concentrate = true];\n");
+      sb.append(" center=true;\n");
+      sb.append(" fontsize=6;\n");
+      sb.append(" node [ color=blue,shape=\"box\"fontsize=6,fontcolor=black,fontname=Arial];\n");
+      sb.append(" edge [ color=black,fontsize=6,fontcolor=black,fontname=Arial];\n");
+      for (IMethod m : set) {
+        if (Util.isAppClass(m.getDeclaringClass())) {
+          sb.append(m);
+          sb.append(" -> ");
+          sb.append(method);
+          sb.append("\n");
+        }      
+      }
+      sb.append("}\n");
+      
+      /**
+       * results.dot
+       */
+      File dotFile = new File("/tmp/results.dot");
+      String strPdfFile= "/tmp/results.pdf";
+      FileWriter fw = new FileWriter(dotFile);
+      fw.append(sb);
+      fw.flush();
+      fw.close();
+      DotUtil.spawnDot(Util.getStringProperty("dotPath"), strPdfFile, dotFile);
+      
+    }
   }
 }
